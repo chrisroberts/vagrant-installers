56f60c3faa037df03892911369492ff0e57fd51a
Chris Roberts <code@chrisroberts.org>
2017-02-22 14:36:04 -0800

    Add some retries to brew install

--- substrate/vagrant-scripts/osx.sh
+++ substrate/vagrant-scripts/osx.sh
@@ -5,7 +5,22 @@ nc -z -w3 192.168.1.1 8123 && export http_proxy="http://192.168.1.1:8123"
 
 gem install json_pure -v '~> 1.0' --no-ri --no-rdoc
 gem install puppet -v '~> 3.0' --no-ri --no-rdoc
-TRAVIS=1 su vagrant -l -c 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+
+result=1
+attempts=0
+while [ "${result}" -ne "0" ]
+do
+    TRAVIS=1 su vagrant -l -c 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+    attempts=$(expr $attempts + 1)
+    if [ $attempts -gt 5 ]
+    then
+        echo "Failed to install homebrew!"
+        exit 1
+    else
+        sleep 2
+    fi
+done
+
 mkdir -p /vagrant/substrate-assets
 chmod 755 /vagrant/substrate/run.sh
 
