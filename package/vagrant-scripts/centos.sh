7b37775b219a8515951d6ba98f26ab7de6344dc2
Chris Roberts <code@chrisroberts.org>
2017-02-22 14:36:32 -0800

    Include extra packages

--- package/vagrant-scripts/centos.sh
+++ package/vagrant-scripts/centos.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
 yum install -y nc curl zip unzip
+
+REPO_RPM_URL="http://yum.puppetlabs.com/puppetlabs-release-el-5.noarch.rpm"
+rm -f /tmp/puppet.rpm
+curl -o /tmp/puppet.rpm -L $REPO_RPM_URL
+rpm -i /tmp/puppet.rpm
+yum install -y puppet ruby-devel
+
 yum groupinstall -yq "Development Tools"
+gem install json -v '~> 1.8.6' --no-ri --no-rdoc
 gem install fpm -v '~> 0.4.0' --no-ri --no-rdoc
 
 # if the proxy is around, use it
--- package/vagrant-scripts/ubuntu.sh
+++ package/vagrant-scripts/ubuntu.sh
@@ -4,7 +4,8 @@
 nc -z -w3 192.168.1.1 8123 && export http_proxy="http://192.168.1.1:8123"
 
 DEBIAN_FRONTEND=noninteractive apt-get update -yq
-DEBIAN_FRONTEND=noninteractive apt-get install -yq build-essential ruby-dev zip unzip
+DEBIAN_FRONTEND=noninteractive apt-get update -yq
+DEBIAN_FRONTEND=noninteractive apt-get install -yq build-essential ruby ruby-dev rubygems1.8 zip unzip
 gem install json -v '~> 1.8.6' --no-ri --no-rdoc
 gem install fpm -v '~> 0.4.0' --no-ri --no-rdoc
 ln -s /var/lib/gems/1.8/bin/fpm /usr/local/bin/fpm
