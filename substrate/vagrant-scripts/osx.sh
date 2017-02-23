1654afb34c47e94983f15190ee6f6bf34a7b095e
Chris Roberts <code@chrisroberts.org>
2017-02-22 19:59:48 -0800

    Disable HTTP pull for brew install

--- substrate/vagrant-scripts/osx.sh
+++ substrate/vagrant-scripts/osx.sh
@@ -10,7 +10,7 @@ result=1
 attempts=0
 while [ "${result}" -ne "0" ]
 do
-    TRAVIS=1 su vagrant -l -c 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+    TRAVIS=1 su vagrant -l -c 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install | sed 's%https://github%git://github%g')"'
     result=$?
     attempts=$(expr $attempts + 1)
     if [ $attempts -gt 5 ]
