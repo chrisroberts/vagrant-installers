4b47c062534eb201c556a74e37f0439d81db543a
Chris Roberts <croberts@hashicorp.com>
2019-06-08 07:31:30 -0700

    .

--- package/vagrant-scripts/centos.sh
+++ package/vagrant-scripts/centos.sh
@@ -7,6 +7,9 @@ nc -z -w3 192.168.1.1 8123 && export http_proxy="http://192.168.1.1:8123"
 mkdir -p /vagrant/substrate-assets
 chmod 755 /vagrant/package/package.sh
 
+yum -d 0 -e 0 -y install chrpath gcc make perl
+yum -d 0 -e 0 -y install perl-Data-Dumper
+
 set -e
 
 /vagrant/package/package.sh /vagrant/substrate-assets/substrate_centos_$(uname -m).zip master
